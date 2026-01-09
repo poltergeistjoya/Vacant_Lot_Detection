7a2d91fb82eb473ba2dd3ee3ee2ffee989079186
poltergeistjoya <joyaranidebi@gmail.com>
2026-01-08 20:34:22 -0500

    formalize load and sample

--- gee_utils.py
+++ gee_utils.py
@@ -4,59 +4,12 @@ from typing import Optional, List
 
 
 from logger import get_logger
-from config import EarthEngineConfig, Config
+from config import Config, GCPConfig
 
 
 log = get_logger()
 
-# import os
-# import json
-# import google.auth
-# from google.auth.transport.requests import Request
-
-# def log_adc_identity():
-#     adc_path = os.path.expanduser("~/.config/gcloud/application_default_credentials.json")
-#     email = None
-#     client_id = None
-#     project = None
-#     auth_type = None
-
-#     # Try standard ADC load first
-#     try:
-#         credentials, project = google.auth.default()
-#         credentials.refresh(Request())
-#         auth_type = type(credentials).__name__
-#         if hasattr(credentials, "service_account_email"):
-#             email = credentials.service_account_email
-#         elif hasattr(credentials, "_subject"):
-#             email = credentials._subject
-#     except Exception:
-#         pass
-
-#     # Fallback: read the JSON manually
-#     if not email and os.path.exists(adc_path):
-#         try:
-#             with open(adc_path, "r") as f:
-#                 adc_json = json.load(f)
-#                 email = adc_json.get("client_email")
-#                 client_id = adc_json.get("client_id")
-#         except Exception as e:
-#             log.error(f"⚠️ Failed to read ADC file: {e}")
-
-#     log.info("🔍 Google ADC info:")
-#     log.info(f"   Auth type: {auth_type or 'unknown'}")
-#     log.info(f"   Project: {project or 'unknown'}")
-#     log.info(f"   Client email: {email or 'unknown'}")
-#     log.info(f"   Client ID: {client_id or 'unknown'}")
-
-#     return {
-#         "auth_type": auth_type,
-#         "project": project,
-#         "client_email": email,
-#         "client_id": client_id
-#     }
-
-def init_gee(config:EarthEngineConfig):
+def init_gee(config:GCPConfig):
     """
     Initialize GEE w project-scoped credentials.
     Assumes ADC (Application Default Credentials) are set via `gcloud auth application-default login`
@@ -78,7 +31,6 @@ def export_image_to_gee(
         region: Optional[ee.Geometry] = None, 
         scale: int =1,
         max_pixels: int = 1e13,
-        # config:EarthEngineConfig = None
 ):
     """
     Export ee.Image to Earth Engine Asset
